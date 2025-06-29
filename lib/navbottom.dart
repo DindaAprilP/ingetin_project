3196149a5ca59bbf1df3ae516325480b22ea2a2c
Dinda Apriliani Putri <ndaprilianip@gmail.com>
2025-06-29 08:42:15 +0000

    BUILD APK

---
+++ lib/navbottom.dart
@@ -0,0 +1,49 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:ingetin_project/beranda.dart';
+import 'package:ingetin_project/menu.dart';
+
+class bottomNavigationBar extends StatefulWidget {
+  const bottomNavigationBar({super.key});
+
+  @override
+  State<bottomNavigationBar> createState() => _bottomNavigationBarState();
+}
+
+class _bottomNavigationBarState extends State<bottomNavigationBar> {
+  var _bottomNavIndex = 0;
+
+  final List <Widget> _pages = [
+    Beranda(),
+    Menu(),
+    //TAMBAH NENG HALAMAN PROFIL NEK WS ENEK
+  ];
+
+  final iconList = <IconData>[
+    Icons.home,
+    Icons.edit,
+    Icons.person,
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages [_bottomNavIndex],
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: AnimatedBottomNavigationBar(
+        icons: iconList,
+        activeIndex: _bottomNavIndex,
+        gapLocation: GapLocation.none,
+        backgroundColor: Colors.black,
+        activeColor: Colors.white,
+        inactiveColor: Colors.white,
+        onTap: (index){
+          setState((){
+            _bottomNavIndex = index;
+          }
+          );
+        }
+      ),
+    );
+  }
+}
